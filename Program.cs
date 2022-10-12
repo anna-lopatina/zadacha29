02a8a5f4bac7c70a910ab27dc00051b4ab95303e
konstantinlopatin <lopatinkos@gmail.com>
2022-10-12 23:21:04 +0300

    Решили задачу 29

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] ElementsArray(string input)
+{
+    string[] x = input.Split(", ");
+    int len = 8;
+    if(x.Length < len) 
+    {
+        len = x.Length;
+    }
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(x[i]);
+    }
+    return array;
+}
+
+void PrintArray(int[] mass)
+{
+    Console.Write("[");
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]}");
+    if (i != mass.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }  
+    Console.Write("]"); 
+}
+Console.Clear();
+
+Console.WriteLine("Введите ряд чисел, разделенных запятой: ");
+string input = Console.ReadLine();
+int[] ArrayResult = ElementsArray(input);
+PrintArray(ArrayResult);
